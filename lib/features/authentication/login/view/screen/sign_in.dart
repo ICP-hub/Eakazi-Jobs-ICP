ab2ba4977a9f4348c751f83c6918b9cf781d06ec
Somya_Sahu <somyasahu10@gmail.com>
2024-01-10 00:53:06 +0530

    Fixed SignIn Routing

--- lib/features/authentication/login/view/screen/sign_in.dart
+++ lib/features/authentication/login/view/screen/sign_in.dart
@@ -35,7 +35,8 @@ import 'package:sizer/sizer.dart';
 import 'package:eakazijobs/interactions.dart';
 import 'package:eakazijobs/integrations.dart';
 import 'package:eakazijobs/init.dart';
-import 'package:eakazijobs/features/authentication/login/view/screen/sign_in.dart' as signIn;
+import 'package:eakazijobs/features/authentication/login/view/screen/sign_in.dart'
+    as signIn;
 import 'package:get/get.dart';
 import 'package:agent_dart/agent_dart.dart';
 
@@ -49,7 +50,7 @@ import '../../../../shared_widgets/options_drop_down.dart';
 import 'dart:convert';
 
 CanisterActor? newActor;
-var fullName;
+var fullName = "John Doe";
 
 class SignIn extends StatefulWidget {
   SignIn({Key? key}) : super(key: key);
@@ -61,18 +62,15 @@ class SignIn extends StatefulWidget {
 class _SignInState extends State<SignIn> {
   String? _error;
   StreamSubscription? _sub;
-  String? _principalIdentity;
   bool _isLoggedIn = false;
   var publicKeyString;
-  // Counter? counter;
   String _decodedDelegation = '';
-  CanisterActor? get actor => newActor;
-
+  Ed25519KeyIdentity? newIdentity;
 
   @override
   void initState() {
     super.initState();
-    ed25519();
+    // ed25519();
     initUniLinks();
   }
 
@@ -91,16 +89,10 @@ class _SignInState extends State<SignIn> {
   void handleLogout() {
     setState(() {
       _isLoggedIn = false;
-      _principalIdentity = null;
     });
   }
 
   // ---------------- Receiving Query Params ----------------
-
-  void printWrapped(String text) {
-    final pattern = new RegExp('.{1,800}'); // 800 is the size of each chunk
-    pattern.allMatches(text).forEach((match) => print(match.group(0)));
-  }
 
   Future<void> initUniLinks() async {
     _sub = uriLinkStream.listen((Uri? uri) async {
@@ -108,10 +100,8 @@ class _SignInState extends State<SignIn> {
         var queryParams = uri.queryParameters;
 
         String delegationString = queryParams['del'].toString();
-        printWrapped("DelegationString: $delegationString");
 
         _decodedDelegation = Uri.decodeComponent(delegationString);
-        printWrapped("Decoded DelegationString: $_decodedDelegation");
 
         DelegationChain _delegationChain =
             DelegationChain.fromJSON(jsonDecode(_decodedDelegation));
@@ -136,77 +126,34 @@ class _SignInState extends State<SignIn> {
             ),
             FieldsMethod.idl);
 
-        // var createUser = await newActor?.getFunc(FieldsMethod.createUser)?.call([
-        //   'John Doe',
-        //   'abc@gmail.com',
-        //   'freelancer',
-        // ]);
-        //
-        // print("Create User: $createUser");
-
-        // final actorController = Get.find<ActorController>();
-        // actorController.setActor(newActor!);
-
-        // var res = await newActor?.getFunc(CounterMethod.whoamI)?.call([]);
-        // print("WhoAmI : $res");
-
-        // var checkUser = await newActor?.getFunc(CounterMethod.checkUser)?.call([]);
-        // Get.toNamed(Routes.signup);
-        // var path = queryParams['path'];
-        //
-        // if ( path != null && path.isNotEmpty ) {
-        //   Get.toNamed(path);
-        // }
-        //
         var checkUser = false;
 
         if (checkUser == true) {
-          Get.toNamed(Routes.home);
+          customLoader.showLoader('Welcome back, please wait...');
+          Future.delayed(Duration(seconds: 2), () {
+            customLoader.dismissLoader();
+            Get.toNamed(Routes.home);
+          });
         } else {
-          Get.toNamed(Routes.signup);
+          customLoader
+              .showLoader('Hey you are new here, please enter your details');
+          Future.delayed(Duration(seconds: 2), () {
+            customLoader.dismissLoader();
+            Get.toNamed(Routes.signup);
+          });
         }
-
-        // var checkUser = await newActor?.getFunc(CounterMethod.checkUser)?.call([]);
-
-        // bool loginStatus = true;
-
-        // bool loginStatus = queryParams['status'] == 'true';
-        //
-        // if (loginStatus) {
-        //   processDelegation(queryParams);
-        // }
-
-        //     setState(() {
-        //       _isLoggedIn = loginStatus;
-        //       if (loginStatus) {
-        //         _principalIdentity = queryParams['principal'] ?? 'Not available';
-        //         _error = queryParams['error'] ?? '';
-        //       } else {
-        //         _principalIdentity = null;
-        //       }
-        //     });
-        //   }
-        // }, onError: (err) {
-        //   print('Error processing incoming URI: $err');
       }
     });
-  }
-
-  // ---------------- Generating ED25519 Key ----------------
-  Ed25519KeyIdentity? newIdentity;
-  Future<void> ed25519() async {
-    newIdentity = await Ed25519KeyIdentity.generate(null);
-    Ed25519PublicKey publicKey = newIdentity!.getPublicKey();
-    var publicKeyDer = publicKey.toDer();
-    publicKeyString = bytesToHex(publicKeyDer);
-
-    print("Public Key: $publicKeyString");
   }
 
   // ---------------- Authentication ----------------
   Future<void> authenticate() async {
     try {
-      // ----- Port : 4943 -----
+      newIdentity = await Ed25519KeyIdentity.generate(null);
+      Ed25519PublicKey publicKey = newIdentity!.getPublicKey();
+      var publicKeyDer = publicKey.toDer();
+      publicKeyString = bytesToHex(publicKeyDer);
+
       const baseUrl = 'http://localhost:4943';
       final url =
           '$baseUrl?sessionkey=$publicKeyString&canisterId=bd3sg-teaaa-aaaaa-qaaba-cai';
@@ -235,66 +182,6 @@ class _SignInState extends State<SignIn> {
   SigninModel signinModel = SigninModel();
   AuthService authService = AuthService();
   CustomLoader customLoader = CustomLoader();
-  //  var authClientCreateOptions = AuthClientLoginOptions()
-  // var createUrlOptions = CreateUrlOptions(
-  //   redirectUri: Uri(
-  //     scheme: "https",
-  //     path: "login.html",
-  //     host: "https://identity.ic0.app/#authorize",
-  //   )
-  //   publicKey: publicKey, // of session key
-  //    scope: scope
-  //    )
-
-  //   var authClient = AuthClient(
-  //   scheme: "rubaru",
-  //   path: "auth",
-  //   authFunction: (AuthPayload payload) async {
-  //     Uri uri = Uri(
-  //       scheme: "https",
-  //       path: "login.html",
-  //       host: "https://identity.ic0.app/#authorize",
-  //       queryParameters: {
-  //         "authUrl": payload.url,
-  //       },
-  //     );
-  //     String result = await FlutterWebAuth.authenticate(
-  //       url: uri.toString(),
-  //       callbackUrlScheme: "rubaru",
-  //     );
-  //     return result;
-  //   },
-  // );
-
-  // @override
-  // submitSecond(context) async {}
-  //
-  // @override
-  // submit(context) async {
-  //   try {
-  //     if (!_formKey.currentState!.validate()) {
-  //       return;
-  //     }
-  //     _formKey.currentState!.save();
-  //     customLoader.showLoader('Login you in...');
-  //
-  //     final result = await authService.signIn(signinModel);
-  //     print('result $result');
-  //     customLoader.dismissLoader();
-  //
-  //     Get.toNamed(Routes.home);
-  //     // if (signupModel.role == 'freelancer') {
-  //     //   Get.toNamed(Routes.freeLancerHome);
-  //     // }
-  //
-  //     // Get.to((CompleteRegistration()));
-  //     // Navigator.pushNamed(context, CompleteRegistrationRoute);
-  //
-  //     // Get.to(Tabs());
-  //   } catch (e) {
-  //     customLoader.showError(e);
-  //   }
-  // }
 
   validateFields(c) {
     print('validating fields');
@@ -378,87 +265,8 @@ class _SignInState extends State<SignIn> {
                       child: SingleChildScrollView(
                         child: Column(
                           children: [
-                            // InputTextNormal(
-                            //     // controller: signUpCtrl.usernameController,
-                            //     autovalidateMode:
-                            //         AutovalidateMode.onUserInteraction,
-                            //     // validator: signUpCtrl.usernameValidator,
-                            //     hintText: "Email address",
-                            //     textInputAction: TextInputAction.next,
-                            //     onSave: (String? value) {
-                            //       signinModel.email = value;
-                            //     },
-                            //     validator: (String? value) {
-                            //       if (value == '') {
-                            //         return '';
-                            //       }
-                            //     },
-                            //     keyboardType: TextInputType.emailAddress),
-                            // const SizedBox(
-                            //   height: 24,
-                            // ),
-                            // InputPasswordText(
-                            //     // controller: signUpCtrl.passwordController,
-                            //     autovalidateMode:
-                            //         AutovalidateMode.onUserInteraction,
-                            //     // validator: signUpCtrl.passwordValidtor,
-                            //     hintText: "Password",
-                            //     textInputAction: TextInputAction.next,
-                            //     onSave: (String? value) {
-                            //       signinModel.password = value;
-                            //     },
-                            //     validator: (String? value) {
-                            //       if (value == '' || value!.length < 6) {
-                            //         return 'Password must be minimum of 6 characters';
-                            //       }
-                            //     },
-                            //     keyboardType: TextInputType.visiblePassword),
-                            // Align(
-                            //   alignment: Alignment.topRight,
-                            //   child: Text(
-                            //     "Forgot your password?",
-                            //     style: textTheme(context).subtitle1?.copyWith(
-                            //         color: colorScheme(context).secondary.withOpacity(0.5)),
-                            //   ),
-                            // ),
-
-                            // const SizedBox(
-                            //   height: 10,
-                            // ),
-                            // Align(
-                            //   alignment: Alignment.topRight,
-                            //   child: Text(
-                            //     "Forgot your password?",
-                            //     style: textTheme(context).subtitle1?.copyWith(
-                            //         color: colorScheme(context)
-                            //             .secondary
-                            //             .withOpacity(0.5)),
-                            //   ),
-                            // ),
-                            // SizedBox(
-                            //   height: 50,
-                            // ),
                             Column(
                               children: [
-                                // Padding(
-                                //   padding: const EdgeInsets.only(
-                                //       left: 16, right: 16),
-                                //   child: AuthBtn(
-                                //     isComplete: c.isValidated.value == true
-                                //         ? true
-                                //         : false,
-                                //     onPressed: () {
-                                //       submit(context);
-                                //       // Get.toNamed(Routes.freeLancerHome);
-                                //       // Loader<void>(context)
-                                //       //     .simpleLoader(() => signUpCtrl.signUp());
-                                //     },
-                                //     text: "Sign In",
-                                //   ),
-                                // ),
-
-                                // add internet identity signin
-                                // customDropDown, themes, buttons,badges, messagelistcontiner
                                 const SizedBox(
                                   height: 50,
                                 ),
@@ -471,16 +279,19 @@ class _SignInState extends State<SignIn> {
                                 const SizedBox(
                                   height: 10,
                                 ),
-
                                 Padding(
-                                  padding: const EdgeInsets.only(left: 24, right: 24),
+                                  padding: const EdgeInsets.only(
+                                      left: 24, right: 24),
                                   child: AuthBtn(
                                     isComplete: c.isValidated.value,
-                                    onPressed: handleLogin, // Change here: Pass the handleLogin function
+                                    onPressed: handleLogin,
                                     text: "Sign in with Internet Identity",
                                     borderRadius: BorderRadius.circular(12),
                                     gradient: LinearGradient(
-                                      colors: [Color(0xFF522785), Color(0xFFED1E79)],
+                                      colors: [
+                                        Color(0xFF522785),
+                                        Color(0xFFED1E79)
+                                      ],
                                       begin: Alignment.topLeft,
                                       end: Alignment.bottomRight,
                                     ),
@@ -489,45 +300,12 @@ class _SignInState extends State<SignIn> {
                                     ),
                                   ),
                                 ),
-
-                                // const SizedBox(
-                                //   height: 6,
-                                // ),
-                                InkWell(
-                                  onTap: () {
-                                    Get.toNamed(Routes.signup);
-                                  },
-                                  child: RichText(
-                                    text: TextSpan(
-                                      text: 'Dont have an account',
-                                      style: textTheme(context)
-                                          .subtitle1
-                                          ?.copyWith(
-                                              color: Colors.black
-                                                  .withOpacity(0.3)),
-                                      children: [
-                                        TextSpan(
-                                          text: "  Signup",
-                                          style: textTheme(context)
-                                              .subtitle1
-                                              ?.copyWith(
-                                                  color: colorScheme(context)
-                                                      .primary),
-                                        )
-                                      ],
-                                    ),
-                                  ),
-                                ),
-                                SizedBox(
-                                  height: 2.h,
-                                ),
                               ],
                             ),
                           ],
                         ),
                       ),
                     )),
-                // const Spacer(),
               ],
             ),
           ),
